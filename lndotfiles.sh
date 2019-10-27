ac81ea9f0e36ef29a7731f4affdab63b70fa00aa
Frederic Noguier <noguier@netguardians.ch>
2019-10-27 16:56:26 +0100

    add tmux conf + generalize lndotfiles script

---
+++ lndotfiles.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+[[ -z $backup_suffix ]] && backup_suffix=before_frno_dotfiles
+
+declare -a directories
+directories=($@)
+
+if [ -z $directories ] ; then
+	for f in * ; do
+		if [ -d $f ] ; then
+			directories+=($f)
+		fi
+	done
+fi
+for dir in "${directories[@]}"; do
+	if [ -d $dir ] ; then
+		pushd . > /dev/null
+		cd $dir
+		if [ -r dotfiles ]; then
+			echo "Creating symlinks for $dir"
+			while read df; do
+				if [ -w ~/$df ]; then
+					mv -f ~/$df ~/$df.$backup_suffix
+				fi
+				ln -s $(pwd)/$df ~/$df
+				echo "created symlink for $df"
+			done < dotfiles
+		fi
+		popd > /dev/null
+	fi
+done
