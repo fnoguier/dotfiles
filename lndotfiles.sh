137a154bb78b5b597c1b1cd2b033646dae286e89
Frederic Noguier <noguier@netguardians.ch>
2019-10-28 18:36:27 +0100

    fix potential script problems

--- lndotfiles.sh
+++ lndotfiles.sh
@@ -5,25 +5,25 @@
 declare -a directories
 directories=($@)
 
-if [ -z $directories ] ; then
+if [ -z "$directories" ] ; then
 	for f in * ; do
-		if [ -d $f ] ; then
+		if [ -d "$f" ] ; then
 			directories+=($f)
 		fi
 	done
 fi
 for dir in "${directories[@]}"; do
-	if [ -d $dir ] ; then
+	if [ -d "$dir" ] ; then
 		pushd . > /dev/null
-		cd $dir
+		cd "$dir" || exit 1
 		if [ -r dotfiles ]; then
 			echo "Creating symlinks for $dir"
-			while read df; do
-				if [ -w ~/$df ]; then
-					# mv -f ~/$df ~/$df.$backup_suffix
-					rm ~/$df
+			while read -r df; do
+				if [ -w "$HOME/$df" ]; then
+					# mv -f ~/$df $HOME/$df.$backup_suffix
+					rm "$HOME/$df"
 				fi
-				ln -s $(pwd)/$df ~/$df
+				ln -s "$(pwd)/$df"  "$HOME/$df"
 				echo "created symlink for $df"
 			done < dotfiles
 		fi
